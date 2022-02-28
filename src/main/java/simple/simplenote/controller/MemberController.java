a73315fe4e97c8553858fc3df238a839426ad478
Win_9 <krrr883@gmail.com>
2022-03-01 00:26:56 +0900

    로그인, 회원가입로직 추가

--- src/main/java/simple/simplenote/controller/MemberController.java
+++ src/main/java/simple/simplenote/controller/MemberController.java
@@ -5,12 +5,16 @@ package simple.simplenote.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import simple.simplenote.controller.form.LogInForm;
+import simple.simplenote.controller.form.statusform.LoginStatusForm;
+import simple.simplenote.controller.form.statusform.SignUpStatusForm;
 import simple.simplenote.domain.Member;
-import simple.simplenote.controller.form.AddUserForm;
-import simple.simplenote.controller.form.StatusForm;
 import simple.simplenote.service.MemberService;
 
 
@@ -19,23 +23,58 @@ import simple.simplenote.service.MemberService;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @CrossOrigin(origins = "*", allowedHeaders = "*")
+@Slf4j
 public class MemberController {
 
     private final ObjectMapper objectMapper;
     private final MemberService memberService;
 
 
-    @PostMapping("")
+    @PostMapping("/sign-in")
     @ResponseBody
     @Transactional(readOnly = false)
-    public String addMember(@RequestBody AddUserForm addUserForm) throws JsonProcessingException {
+    public ResponseEntity<LoginStatusForm> logInMember(@RequestBody LogInForm logInForm) throws JsonProcessingException {
 
-        System.out.println("MemberController.addMember");
-        Member member = new Member();
-        member.setNickName(addUserForm.getNickname());
+        Member findMember = memberService.findByName(logInForm.getNickname());
+        if (findMember == null){
+            LoginStatusForm loginStatusForm =
+                    new LoginStatusForm(false, false,"NoneExistId");
+            return new ResponseEntity<>(loginStatusForm, HttpStatus.FORBIDDEN);
+        }
 
-        memberService.addMember(member);
+        if (!findMember.getPassWord().equals(logInForm.getPassword())){
+            LoginStatusForm loginStatusForm =
+                    new LoginStatusForm(true, false,"Incorrect Password");
+            return new ResponseEntity<>(loginStatusForm, HttpStatus.FORBIDDEN);
+        }
 
-        return objectMapper.writeValueAsString(new StatusForm("Good Received"));
+        LoginStatusForm loginStatusForm =
+                new LoginStatusForm(true, true,"Correct Id");
+        return new ResponseEntity<>(loginStatusForm, HttpStatus.OK);
+
+    }
+
+    @PostMapping("/sign-up")
+    @ResponseBody
+    @Transactional(readOnly = false)
+    public ResponseEntity<SignUpStatusForm> signUpMember(@RequestBody LogInForm logInForm) throws JsonProcessingException {//회원폼, 로그인폼 동일하기때문에
+
+
+        if (memberService.findByName(logInForm.getNickname()) != null) {
+            SignUpStatusForm SignUpStatusForm = new SignUpStatusForm(false,"Duplicated Id");
+            return new ResponseEntity<>(SignUpStatusForm, HttpStatus.FORBIDDEN);
+        }
+
+        log.info("logInMember={}",logInForm.getNickname());
+
+        Member signUpMember = new Member();
+
+        signUpMember.setNickName(logInForm.getNickname());
+        signUpMember.setPassWord(logInForm.getPassword());
+
+        memberService.addMember(signUpMember);
+        log.info("memberId={}",signUpMember.getNickName());
+
+        return new ResponseEntity<>(new SignUpStatusForm(true, "Registered"), HttpStatus.OK);
     }
 }
